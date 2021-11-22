20ea26afccbdf472fd02348477156cbd6c98ff24
Milan Das <milan.das@intel.com>
2021-11-22 05:47:12 +0530

    update pattern learning

--- Pattern_Printing.java
+++ Pattern_Printing.java
@@ -1,5 +1,31 @@
+//approach to solve any pattern related programming question
+/*Following should be the approach:
+        1. No. of Lines = No. of Rows = No. of times outer forloop will run
+        2. Identify for every row number - How many columns are present and type of elements in the column
+        3. What do you need to print
+        
+        E.g. *
+             * * 
+             * * *
+             * * * *
+             
+        In the above pattern, we need to find out 1, 2, 3
+        1. No. of Row = 4, so number of times outer for loop with run = 4
+        2. For row 1 we have 1 column, for row 2 we have 2 columns ... so on
+        3. We need to print "*"
+         
+*/
 public class PatternLearning {
     
+    /*Patter 1: 
+    
+        Print the following pattern
+    
+        * * * *
+        * * *
+        * * 
+        *
+    */
     static void pattern1(int val){
         
         for(int row=1; row<=val; row++)
@@ -12,6 +38,16 @@ public class PatternLearning {
             System.out.println();
         }
     }
+    
+    /*Patter 2: 
+    
+        Print the following pattern
+    
+        *
+        * * 
+        * * *
+        * * * *
+    */
     
     static void pattern2(int val){
         
@@ -27,8 +63,9 @@ public class PatternLearning {
     }
     public static void main(String args[]) {
       
-        pattern1(5);
-        pattern2(5);
+        //Draw the pattern by passing the row count to the function:
+        pattern1(4);
+        pattern2(4);
       
     }
 }
